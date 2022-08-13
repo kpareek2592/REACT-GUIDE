dccba7516f58b70eb3eeffee4c7f4f69b3fab8c6
Kaushal <kpareek2592@gmail.com>
2022-08-14 01:12:49 +0530

    component composition (props.children)

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,15 +1,16 @@
+import Card from "./Card";
 import ExpenseItem from "./ExpenseItem";
 import './Expenses.css';
 
 function Expenses(props) {
     return (
-      <div className="expenses">
+      <Card className="expenses">
         <ExpenseItem
           title={props.expenses[0].title}
           amount={props.expenses[0].amount}
           date={props.expenses[0].date}
         />
-      </div>
+      </Card>
     );
 }
 
