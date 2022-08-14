9d45fe679cf480efa27285daca70eca3bd9b0f9e
Kaushal <kpareek2592@gmail.com>
2022-08-14 13:24:19 +0530

    Listening to user inputs with multiple states

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,28 +1,45 @@
-import React from "react";
-import './ExpenseForm.css';
+import React, { useState } from "react";
+import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
-    return (
-      <form>
-        <div className="new-expense__controls">
-          <div className="new-expense__control">
-            <label>Title</label>
-            <input type="text" />
-          </div>
-          <div className="new-expense__control">
-            <label>Amount</label>
-            <input type="number" min="0.01" step="0.01" />
-          </div>
-          <div className="new-expense__control">
-            <label>Date</label>
-            <input type="date" min="2019-01-01" step="2022-12-31" />
-          </div>
+
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredAmount, setEnteredAmount] = useState("0.01");
+  const [enteredDate, setEnteredDate] = useState("2019-01-01");
+
+  const titleChangeHandler = (event) => {
+    setEnteredTitle(event.target.value);
+  };
+
+  const amountChangeHandler = (event) => {
+    setEnteredAmount(event.target.value);
+  };
+
+  const dateChangeHandler = (event) => {
+    setEnteredDate(event.target.value);
+  };
+
+  return (
+    <form>
+      <div className="new-expense__controls">
+        <div className="new-expense__control">
+          <label>Title</label>
+          <input type="text" onChange={titleChangeHandler} />
         </div>
-        <div className="new-expense__actions">
-          <button type="submit">Add Expense</button>
+        <div className="new-expense__control">
+          <label>Amount</label>
+          <input type="number" min="0.01" step="0.01" onChange={amountChangeHandler}/>
         </div>
-      </form>
-    );
+        <div className="new-expense__control">
+          <label>Date</label>
+          <input type="date" min="2019-01-01" step="2022-12-31" onChange={dateChangeHandler}/>
+        </div>
+      </div>
+      <div className="new-expense__actions">
+        <button type="submit">Add Expense</button>
+      </div>
+    </form>
+  );
 };
 
 export default ExpenseForm;
